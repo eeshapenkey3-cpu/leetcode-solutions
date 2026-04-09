0a8b55d0dc91b8e282fc6131ea701002ce76ea48
eeshapenkey3-cpu <eeshapenkey3@gmail.com>
2026-04-09 11:57:20 +0100

    Time: 4140 ms (5.01%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1415-students-and-examinations/students-and-examinations.sql
@@ -0,0 +1,8 @@
+SELECT s.student_id, s.student_name, su.subject_name, COUNT(e.student_id) attended_exams FROM Students AS s
+CROSS JOIN Subjects AS su
+LEFT JOIN Examinations AS e
+ON s.student_id = e.student_id
+AND su.subject_name = e.subject_name
+GROUP BY s.student_id, s.student_name, su.subject_name
+ORDER BY s.student_id, s.student_name, su.subject_name
+;
