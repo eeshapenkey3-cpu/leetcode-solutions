f101ded3153edd3c5f1c5d249d7d604ce5fad6e2
eeshapenkey3-cpu <eeshapenkey3@gmail.com>
2026-05-25 11:26:25 +0100

    Time: 0 ms (100.00%) | Memory: 42.3 MB (98.22%) - LeetSync

---
+++ 1205-defanging-an-ip-address/defanging-an-ip-address.java
@@ -0,0 +1,16 @@
+class Solution {
+
+    public String defangIPaddr(String address) {
+
+        StringBuffer sb = new StringBuffer();
+        for(int i = 0; i < address.length(); i++){
+            if(address.charAt(i) == '.'){
+                sb.append("[.]");
+            }else{
+                sb.append(address.charAt(i));
+            }
+        }//end for loop
+        return sb.toString();
+    }
+
+}
